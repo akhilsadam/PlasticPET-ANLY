baf1534807dcc3a0f95b40ad754c4877c01d9ce3
Akhil Sadam <sadam.akhil@gmail.com>
2021-03-11 15:33:03 -0600

    rm gitignore

--- analyze.py
+++ analyze.py
@@ -18,7 +18,6 @@ from tqdm import tqdm
 import multiprocessing
 import cv2
 import sys
-
 #------------------------------------------------------------------------------------
 PYTHONIOENCODING="UTF-8"  #sys.setdefaultencoding("ISO-8859-1")
 #------------------------------------------------------------------------------------
